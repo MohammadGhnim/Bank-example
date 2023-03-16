207391a6da3fffa2c9df858427e40eec9f03e65d
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-16 23:23:23 +0100

    more-steps3

--- bank-example.py
+++ bank-example.py
@@ -19,27 +19,39 @@ class Bank:
 
 
     def withdrow(self, amount):
-        self.balance -= amount
-        print(f'Your current balance : {self.balance}')
+        if (self.balance - amount) < -501:
+            print(self.balance - amount)
+            print(f'you don not have enough balance : {self.balance}')
+        else:
+            self.balance -= amount
+            self.show_balance()
+        
 
     def deposite(self, amount):
         self.balance += amount
-        print(f'Your current balance : {self.balance}')
+        self.show_balance()
 
 
     def show_details(self):
         print(f'Name : {self.name}')
-        print(f'Age : {self.age}')
+        print(f'Age  : {self.age}')
 
     def show_balance(self):
         print(f'Your current balance : {self.balance}')
+
+        
 c1 = Bank('ahmad', 23)
 
-c1.deposite(500)
 c1.withdrow(200)
+c1.withdrow(200)
+c1.withdrow(100)
+
 c1.show_details()
 c1.show_balance()
 
+
+
+'''
 #add more client
 
 c2 = Bank('hassan', 25)
@@ -47,3 +59,4 @@ c2.deposite(1000)
 c2.withdrow(100)
 c2.show_details()
 c2.show_balance()
+'''
