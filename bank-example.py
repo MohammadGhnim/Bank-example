d2284044c65da3527b7d9b5850a21e155a055539
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-16 22:39:05 +0100

    more-steps

--- bank-example.py
+++ bank-example.py
@@ -12,6 +12,8 @@ Bank example:
 class Bank:
     def __init__(self, name, age):
         print(f'welcome {name}')
+        self.name = name
+        self.age = age
         self.balance = 0
 
 
@@ -24,8 +26,12 @@ class Bank:
         self.balance += amount
         print(f'Your current balance : {self.balance}')
 
-c1= Bank('ahmad', 23)
+
+    def show_details(self):
+        print(f'Name : {self.name}')
+        print(f'Age : {self.age}')
+c1 = Bank('ahmad', 23)
 
 c1.deposite(500)
 c1.withdrow(200)
-    
+c1.show_details() 
