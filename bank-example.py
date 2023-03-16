c1d0d82918f19d1c6510eaa63e30657178f436e5
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-16 22:30:17 +0100

    first-steps

--- bank-example.py
+++ bank-example.py
@@ -8,3 +8,24 @@ Bank example:
       - show balance
 
 '''
+
+class Bank:
+    def __init__(self, name, age):
+        print(f'welcome {name}')
+        self.balance = 0
+
+
+
+    def withdrow(self, amount):
+        self.balance -= amount
+        print(f'Your current balance : {self.balance}')
+
+    def deposite(self, amount):
+        self.balance += amount
+        print(f'Your current balance : {self.balance}')
+
+c1= Bank('ahmad', 23)
+
+c1.deposite(500)
+c1.withdrow(200)
+    
